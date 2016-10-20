3014edef489757b44b7e170c5b9a49a7cdf89425
ivorra <divorrap@gmail.com>
2016-10-21 01:42:29 +0800

    FriendProfileView design

--- UNote/FriendProfileViewController.swift
+++ UNote/FriendProfileViewController.swift
@@ -8,12 +8,52 @@
 
 import UIKit
 
-class FriendProfileViewController: UIViewController {
+class FriendProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet weak var profileBackground: UIImageView!
+    @IBOutlet weak var profilePicture: UIImageView!
+    
+    // data source
+    private var numberOfFolders = 9
+    
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numberOfFolders
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let folderCell = collectionView.dequeueReusableCell(withReuseIdentifier: "FolderCell", for: indexPath) as UICollectionViewCell
+        
+        // Set File image
+        let img = folderCell.contentView.viewWithTag(1) as! UIImageView
+        img.image = UIImage(named: "Folder")
+        
+        // Set Subject name
+        let value = folderCell.contentView.viewWithTag(2) as! UILabel
+        value.text = "EE4304"
+        
+        
+        return folderCell
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        profilePicture.layer.borderWidth = 2.0
+        profilePicture.layer.masksToBounds = false
+        profilePicture.layer.borderColor = UIColor.white.cgColor
+        profilePicture.layer.cornerRadius = 13
+        profilePicture.layer.cornerRadius = profilePicture.frame.size.height/2
+        profilePicture.clipsToBounds = true
+        
     }
 
     override func didReceiveMemoryWarning() {
