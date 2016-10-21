c1986d89274396ebd0cca446ccd588e49e10f444
ivorra <divorrap@gmail.com>
2016-10-21 13:00:57 +0800

    Some bugs fixed

---
+++ UNote/SocialViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SocialViewController.swift
+//  UNote
+//
+//  Created by eda on 17/10/2016.
+//  Copyright © 2016 EE4304. All rights reserved.
+//
+
+import UIKit
+
+class SocialViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let friends: [String] = ["Peter","Alice","John","Kenneth","Anna","Julie","Matthew","Jake","Wilson","Sam","Paul"]
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath)
+        
+        cell.textLabel?.text = friends[indexPath.row]
+        
+        
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
