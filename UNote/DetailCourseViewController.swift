be5fb929580cf821fbdaf7a7685085ea4db3f224
kahosung3 <sungkaho@sungkahodemacbook-air.local>
2016-11-02 00:42:22 +0800

    search function on detailed view controller

--- UNote/DetailCourseViewController.swift
+++ UNote/DetailCourseViewController.swift
@@ -14,33 +14,22 @@ class DetailCourseViewController: UIViewController, UICollectionViewDataSource,U
     var list_ready = false
     var filesInThisCourse : NSMutableArray = []
     var thisCourseTitle : String!
+    var searchBarAction: Bool = false
+    @IBOutlet weak var searchBar: UISearchBar!
     
-//    var photoCoreStack = CourseListCore()
-//    var takenPhotoData : [TakenPhoto] = []
-//    var valid2ShownTakenPhotoData : [TakenPhoto] = []
-//    var selectedIndexPath : IndexPath!
     
-    func createSearchBar(){
-        
-        let searchBar = UISearchBar()
-        searchBar.showsCancelButton = false
-        searchBar.placeholder = "Enter search text"
-        searchBar.delegate = self
-        
-    }
+    var dataSource:[String]=[]
+    var dataSourceForSearchResult:[String]=[]
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
+        self.dataSourceForSearchResult = [String]()
         
+        self.searchBar.delegate = self
         
-
-        // Do any additional setup after loading the view.
-        //self.createSearchBar()
         
         self.notePhotoCollection.delegate = self
         self.notePhotoCollection.dataSource = self
@@ -53,7 +42,6 @@ class DetailCourseViewController: UIViewController, UICollectionViewDataSource,U
         
         
         
-        
         let getCourseNameDefault = UserDefaults.standard
         
         if let val = getCourseNameDefault.string(forKey: "SaveCourseNameInDetailController") {
@@ -62,61 +50,6 @@ class DetailCourseViewController: UIViewController, UICollectionViewDataSource,U
             self.thisCourseTitle = val
             getCourseNameDefault.synchronize()
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        /*
-        
-        let data_context = photoCoreStack.persistentContainer.viewContext
-        
-        do{
-        
-            try
-                takenPhotoData = data_context.fetch(TakenPhoto.fetchRequest())
-            
-    
-            
-            
-            if takenPhotoData.count > 0 {
-            
-                for eachNote in takenPhotoData {
-                   
-                    if eachNote.courseName == self.navigationItem.title
-                    {
-                    
-                        
-                        valid2ShownTakenPhotoData.append(eachNote)
-                        
-                        print("this belong to this course")
-                        print(eachNote.courseName)
-                        print(self.navigationItem.title)
-                    
-                        
-                    }else{
-                        print("this note is not belong to this course")
-                    }
-                    
-                }
-            
-            }else{
-                
-                print("no note on this course")
-            }
-            
-            
-            
-        }catch{
-            
-            
-            
-        }
-        */
-        
         
         
     }
@@ -142,17 +75,6 @@ class DetailCourseViewController: UIViewController, UICollectionViewDataSource,U
         // Dispose of any resources that can be recreated.
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
 
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
@@ -163,40 +85,62 @@ class DetailCourseViewController: UIViewController, UICollectionViewDataSource,U
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
         //return self.valid2ShownTakenPhotoData.count no use anymore
-        return filesInThisCourse.count
+     //   return filesInThisCourse.count
+        
+        if self.searchBarAction {
+            return self.dataSourceForSearchResult.count
+        }
+        
+        return self.dataSource.count
+    
+    
+    
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as UICollectionViewCell
-    
-        // Configure the cell
-//    
-//        let img  = cell.contentView.viewWithTag(1) as! UIImageView
-        //get image
+
+        if self.searchBarAction {
+            
+            let note = self.dataSourceForSearchResult[indexPath.row]
+            
+            var img = cell.contentView.viewWithTag(1) as! UIImageView
+            
+            let imag_identity = cell.contentView.viewWithTag(2) as! UILabel
+            
+            img.image = UIImage(named: "Folder")
+            
+            imag_identity.text = note
         
-        //let imgIdentity = cell.contentView.viewWithTag(2) as! UILabel
+        }else if self.searchBarAction == false {
+            
+        
+            
+            
+            let noteItem = dataSource[indexPath.row]
+            
+            print("test hi")
+            print(noteItem)
+            
+            var img = cell.contentView.viewWithTag(1) as! UIImageView
+            
+            let imag_identity = cell.contentView.viewWithTag(2) as! UILabel
+            
+            img.image = UIImage(named: "Folder")
+            
+            
+            imag_identity.text = noteItem
+            
+            
+        }
         
         
-        //imgIdentity.text = self.valid2ShownTakenPhotoData[indexPath.item].noteTitle
-        
-        let noteItem : NSDictionary = filesInThisCourse[indexPath.row] as! NSDictionary
-        
-        var img = cell.contentView.viewWithTag(1) as! UIImageView
-        
-        let imag_identity = cell.contentView.viewWithTag(2) as! UILabel
-        
-        img.image = UIImage(named: "Folder")
-    
-        
-        imag_identity.text = noteItem.object(forKey: c.TAG_FILE_NAME) as! String?
         
         
         
         
-        
-        
-        cell.backgroundColor = UIColor.cyan
+        cell.backgroundColor = UIColor.red
         
         
         return cell
@@ -233,11 +177,12 @@ class DetailCourseViewController: UIViewController, UICollectionViewDataSource,U
     
     func didSetItemWith(_ state: Bool, itemType: String) {
         
-        
+        // may not use in this situation
     }
     
     
     func didGetItemFailedWithError(_ itemType: String, error: String) {
+        
         
     }
     
@@ -280,6 +225,12 @@ class DetailCourseViewController: UIViewController, UICollectionViewDataSource,U
                     
                     filesInThisCourse.add(item)
                     
+                    let str = item?.object(forKey: c.TAG_FILE_NAME) as! String
+                    
+                    dataSource.append(str)
+                    
+                    
+                
                 }else{
                     print("this item is not from this course")
                 }
@@ -287,13 +238,47 @@ class DetailCourseViewController: UIViewController, UICollectionViewDataSource,U
                 DispatchQueue.main.async{
                     
                     self.notePhotoCollection.reloadData()
-                
+                    
+                    
                 }
                 
                 
             }
             
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+//        let controller = UIAlertController(title: "error", message: "EEEEE", preferredStyle: .alert)
+//        
+//        let action = UIAlertAction(title: "fff", style: .cancel, handler: nil)
+//        
+//        
+//        controller.addAction(action)
+//        
+//        self.present(controller, animated: true, completion: nil)
+
+        
+        if searchText.characters.count > 0{
             
+            
+            self.searchBarAction = true
+            
+            self.filterContentForSearchText(searchText: searchText)
+            self.notePhotoCollection.reloadData()
+        }else{
+            
+            self.searchBarAction = false
+            self.notePhotoCollection.reloadData()
             
             
         }
@@ -301,6 +286,81 @@ class DetailCourseViewController: UIViewController, UICollectionViewDataSource,U
         
         
     }
+    
+    
+    
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        
+        
+        self.searchBar.setShowsCancelButton(true, animated: true)
+        
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        
+        self .cancelSearching()
+        
+        self.notePhotoCollection.reloadData()
+    }
+    
+    
+    func cancelSearching(){
+        self.searchBarAction = false
+        self.searchBar!.resignFirstResponder()
+        self.searchBar!.text = ""
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+    
+        if searchText.characters.count > 0 {
+            
+            self.searchBarAction = true
+            self.filterContentForSearchText(searchText: searchText)
+            
+            self.notePhotoCollection.reloadData()
+            
+            
+            
+        }else{
+            
+            self.searchBarAction = false
+            
+            self.notePhotoCollection.reloadData()
+        
+        
+        }
+        
+        
+    }
+    
+    
+    func filterContentForSearchText(searchText:String){
+        self.dataSourceForSearchResult = self.dataSource.filter({ (text:String) -> Bool in
+            return text.contains(searchText)
+        })
+        
+    }
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        self.searchBarAction = true
+        self.view.endEditing(true)
+        
+    }
+    
+    
+    
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        
+        self.searchBarAction = false
+        self.searchBar.setShowsCancelButton(false, animated: false)
+        
+    }
+    
     
     
     func doubleTappedOnView(sender: UITapGestureRecognizer){
