708255943808a176969d4e953ecd907e1f4f4f3f
aranchafoz <aranchunfli11foz@hotmail.com>
2016-11-22 10:51:01 +0800

    Migrate download image to Expand Note View

--- UNote/ExpandNoteViewController.swift
+++ UNote/ExpandNoteViewController.swift
@@ -12,29 +12,109 @@ class ExpandNoteViewController: UIViewController {
     
     @IBOutlet weak var expandImage: UIImageView!
     
+    var userID : String!
+    var courseName : String!
+    var filesSaved:[NSDictionary] = []
+    var selectedImage:UIImage!
+    var dataSourceForSearchResult:[NSDictionary] = []
+    var imageDataSetDict:NSMutableDictionary = [:]
+    
     var toPass:UIImage!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         expandImage.image = toPass
+        
+        
+        userID = UserDefaults.standard.value(forKey: "userID") as! String!
+        courseName = UserDefaults.standard.value(forKey: "course") as! String!
+        
+        
+        filesSaved = UserDefaults.standard.value(forKey: "savedMaterial") as! [NSDictionary]
+        
+        
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(self.longPressed(sender:)))
+        
+        
+        view.addGestureRecognizer(longPressRecognizer)
+        
+        
+        Appdata.sharedInstance.awsEditor?.getUserFilesListTable(self.userID)
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
-    */
+    
+    
+    func didUploadFileSucceedWith(_ fileid: String) {
+        
+    }
+    
+    func didDownloadFileFailedWith(_ fileid: String, error: String) {
+        
+        
+        print("wwwwwww")
+    }
+    
+    
+    func didDownloadFileSucceedWith(_ fileid: String, data: NSData) {
+        
+        print("ssssss")
+        
+        print(fileid)
+        imageDataSetDict[fileid] = data
+        
+        
+        
+    }
+    func didUploadFileFailedWith(_ fileid: String, error: String) {
+        
+        
+        
+    }
+    
+    
+    func longPressed(sender:UILongPressGestureRecognizer){
+        
+        
+        let passImageToSave = self.expandImage.image
+        
+        if sender.state == UIGestureRecognizerState.ended{
+            print("okok")
+            if passImageToSave != nil {
+                
+                
+                UIImageWriteToSavedPhotosAlbum(self.expandImage.image!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+                
+            }
+            
+        }
+    }
+    
+    
+    func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+            print("okooooo")
+        }
+    }
 
 }
